a5bb674823e6d61844b8dbd7f797a4aef2aab1a4
rbueno <rbueno@fazenda.ms.gov.br>
2026-09-21 16:01:55 -0400

    passando o mock de dados para visualização no console

--- src/app/componentes/listas-filmes/listas-filmes.ts
+++ src/app/componentes/listas-filmes/listas-filmes.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+//import { GeneroFilme } from '../genero-filme/genero-filme';
+import { IFilme, GeneroFilme } from '../filme/filmeInterface';
+import { map } from 'rxjs';
+import { filmes } from '../../mock-filmes';
 
 @Component({
   imports: [],
@@ -6,4 +10,52 @@ import { Component } from '@angular/core';
   styleUrl: './listas-filmes.css',
   templateUrl: './listas-filmes.html',
 })
-export class ListasFilmes {}
+export class ListasFilmes implements OnInit {
+  generos: GeneroFilme[] = [];//ta fazendo um array de um genero de filme 
+  filmesPorGeneros: Map<string, IFilme[]> = new Map();
+
+  ngOnInit(){
+    this.filmesPorGeneros = new Map();
+    
+    filmes.forEach((filme) => {
+      const generoId = filme.genero.id 
+      if(!this.filmesPorGeneros.has(generoId)){
+        this.filmesPorGeneros.set(generoId, [])
+      }
+      this.filmesPorGeneros.get(generoId)?.push(filme);
+    })
+  
+    this.generos = 
+    [
+      {
+        id: 'crime',
+        value: 'Crime',
+        filmes: (this.filmesPorGeneros.get("crime") ?? []) as any,
+      },
+      {
+        id: 'drama',
+        value: 'Drama',
+        filmes: (this.filmesPorGeneros.get("drama") ?? []) as any,
+      },
+      {
+        id: 'comedia',
+        value: 'Comedia',
+        filmes: (this.filmesPorGeneros.get("comedia") ?? []) as any,
+      },
+      {
+        id: 'terror',
+        value: 'Terror',
+        filmes: (this.filmesPorGeneros.get("terror") ?? []) as any,
+      },
+      {
+        id: 'acao',
+        value: 'Acao',
+        filmes: (this.filmesPorGeneros.get("acao") ?? []) as any,
+      },
+
+
+    ]
+    console.log(this.filmesPorGeneros)
+  }
+
+}
